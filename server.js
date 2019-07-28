841ca20cc9c6d3346f6f4216019ef0c9ab70dd4c
= <anirudh.b.123@gmail.com>
2019-07-28 16:22:36 +0530

    added url check and random urls

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express'),
-			bodyParser    = require('body-parser'),
-			mongoose = require('mongoose'),
-			Url = require('./urlSchema');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	shortid = require('shortid'),
+	validUrl = require('valid-url'),
+	Url = require('./urlSchema');
 
 const app = express();
 
@@ -17,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const db = 'mongodb://localhost/url_shortner';
 mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Database connected"))
-    .catch(console.log);
+	.then(() => console.log("Database connected"))
+	.catch(console.log);
 
 //=======================
 // ROUTES
@@ -26,32 +28,40 @@ mongoose.connect(db, { useNewUrlParser: true })
 
 app.post('/api/shorten', async (req, res) => {
 	const originalUrl = req.body.url;
-	const shortUrl = req.body.short;
-	if(!originalUrl || !shortUrl){
-		res.json({success: false, error: "Please send two keys: url and short"})
-	} else {
-		const result = await Url.find({shortUrl});
-		if(result.length > 0){
-			res.json({success: false, error: 'url already exists'});
-		} else {
-			const newUrl = new Url({
-				originalUrl,
-				shortUrl
-			});
-			newUrl.save()
-			.then(a => res.json({success: true, newUrl: req.headers.host+'/'+shortUrl}))
-			.catch(err => res.json({success: false, error: err}));
-		}
+	if (!originalUrl) {
+		res.json({ success: false, error: "Url to be shortened not sent" });
+		return;
 	}
+	if (!validUrl.isUri(originalUrl)) {
+		res.json({ success: false, error: 'invalid url' });
+		return;
+	}
+	let shortUrl = req.body.short;
+	// Will generate random url if no custom url is provided
+	if (!shortUrl) {
+		shortUrl = shortid.generate();
+	}
+	const result = await Url.find({ shortUrl });
+	if (result.length > 0) {
+		res.json({ success: false, error: 'url already exists' });
+		return;
+	}
+	const newUrl = new Url({
+		originalUrl,
+		shortUrl
+	});
+	newUrl.save()
+		.then(a => res.json({ success: true, newUrl: req.headers.host + '/' + shortUrl }))
+		.catch(err => res.json({ success: false, error: err }));
 });
 
 app.get('/:url', async (req, res) => {
 	const shortUrl = req.params.url;
-	const result = await Url.findOne({shortUrl});
-	if(result){
+	const result = await Url.findOne({ shortUrl });
+	if (result) {
 		res.redirect(result.originalUrl);
 	} else {
-		res.json({success: false, error: 'no such url'});
+		res.json({ success: false, error: 'no such url' });
 	}
 });
 
@@ -61,5 +71,5 @@ app.get('/:url', async (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('App listening on port ' + port);
+	console.log('App listening on port ' + port);
 });
